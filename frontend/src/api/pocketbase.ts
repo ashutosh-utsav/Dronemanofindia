cb76414efbe774b26a0e33c2e465f310d46a8fe9
Ashutosh Utsav <utsav.ashutosh@gmail.com>
2026-02-25 05:00:22 +0530

    update

--- frontend/src/api/pocketbase.ts
+++ frontend/src/api/pocketbase.ts
@@ -2,6 +2,8 @@
 import PocketBase from 'pocketbase';
 
 // Connect to your local PocketBase server URL
-const pb = new PocketBase('http://127.0.0.1:8090');
+// const pb = new PocketBase('http://127.0.0.1:8090');
+
+const pb = new PocketBase('https://render-droneman-1.onrender.com');
 
 export default pb;
